ce7e30881021a8748a2859c1cd12a403c4d248eb
EricGR <ericgarcia.rodrigues@gmail.com>
2018-06-20 20:54:21 -0300

    arquivo de conexao com o banco (FabricaConexao)

---
+++ FabricaConexao.java
@@ -0,0 +1,43 @@
+package me.agend.modelo.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+	
+	private Connection conexao;
+	
+	public FabricaConexao() {
+		conexao = null;
+	}
+	
+	public Connection getConexao(){
+		
+		if(conexao == null){
+			
+			try{
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				conexao = DriveManager.getConnection("jdbc:mysql://localhost/agendamento", "root", "");
+				
+				return conexao;
+				
+			} catch(ClassNotFoundException e){
+				System.out.println("Falha ao se connectar");
+				e.printStackTrace();
+				return null;
+			} catch (SQLException e){
+				System.out.println("Falha ao se conectar");
+				e.printStackTrace();
+				return null;
+			}
+			
+		} else {
+			
+			return conexao;
+		}
+		
+	}
+	
+}
